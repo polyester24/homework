89dc8c5bf31d1f4de22a8368ee822b496d448a46
poly <aoi.itabashi@softage.co.jp>
2026-05-03 11:54:26 +0900

    Ex_04_02

---
+++ Ex_04_02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex_04_02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("年を入力してください->");
+        int year = int.Parse(Console.ReadLine()!);
+
+        if (DateTime.IsLeapYear(year))
+    {
+        Console.WriteLine($"{year}は閏年です。");
+    }
+    else
+        {
+        Console.WriteLine($"{year}は閏年ではありません。");
+    }
+        }
+
+    }
